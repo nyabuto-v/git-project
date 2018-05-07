8380e83630a683fdf69121b76f72d4c3ae97001e
Nyabuto Vice <nyabutovice@gmail.com>
2018-05-07 10:34:17 +0300

    initial commit

---
+++ src/app/profile.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+@Injectable()
+export class ProfileService {
+
+  private username:string;
+  private clientid = '88349eee8071659d6a69';
+  private clientsecret = '4fd3e72b5d0c887d7f0deb0cf8814f585bbcf1c0';
+
+  constructor(private http:Http) { 
+    console.log("service is now ready!");
+    this.username = 'nyabuto-v';
+}
+
+getProfileInfo(){
+  return this.http.get("http://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret).map(res => res.json());
+  }
+
+}
