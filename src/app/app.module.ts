ee995d72a15f53aed5a85b385a88f75fe4272d0a
Nyabuto Vice <nyabutovice@gmail.com>
2018-05-16 17:07:43 +0300

    update the root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule} from '@angular/http';
+import{ FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -14,7 +15,8 @@ import { ProfileService } from './profile.service';
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FormsModule
   ],
   providers: [ProfileService ],
   bootstrap: [AppComponent]
