5359ddadff09213ac95ef942a342fe1afb64fae6
Nyabuto Vice <nyabutovice@gmail.com>
2018-05-16 16:38:23 +0300

    add function for finding profile

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -19,4 +19,8 @@ getProfileInfo(){
 getProfileRepos(){
   return this.http.get("http://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret).map(res => res.json());
   }
+
+  updateProfile(username:string){
+    this.username = username;
+  }
 }
